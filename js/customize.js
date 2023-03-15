46244b4da9046c2fef0b4427b718ebc5c55a1086
Tseng <tseng7510@gmail.com>
2023-03-15 11:12:01 +0800

    更新tab功能，畫面小時變手風琴

--- js/customize.js
+++ js/customize.js
@@ -2,7 +2,7 @@
 topNav(); // 手機版顯示nav選單
 // navSticky(); // 捲動時固定主選單
 fatFooter(); // fatFooter是否要展開
-tabFunction('.tabSet'); // tab功能
+// tabFunction('.tabSet'); // tab功能
 scrollTables('table'); // table捲動功能
 fontSize();
 tableAddDataAttributes({
@@ -237,3 +237,248 @@ accordionSlider({
     },
   });
 })();
+
+function TabFunction2() {
+  'use strict';
+  let openIndex = 1 - 1;
+  const tab = document.querySelector('.tabSet2');
+  let desktopTabItems;
+
+  function init() {
+    const tabItem = tab.querySelectorAll('.tabItems .tabBtn');
+    const contentItem = tab.querySelectorAll('.tabContent');
+
+    tabItem.forEach((item, index) => {
+      const random = `tab_${randomLetter(4)}${randomFloor(0, 9999)}`;
+      const text = item.innerHTML;
+      item.setAttribute('aria-selected', 'false');
+      item.setAttribute('role', 'tab');
+      item.setAttribute('aria-controls', random);
+      // item.setAttribute('aria-selected', 'false');
+      contentItem[index].setAttribute('aria-labelledby', random);
+      contentItem[index].setAttribute('aria-label', text);
+    });
+    desktopTabItems = tab.querySelector('.tabItems').cloneNode(true);
+  }
+
+  function a11yControlMobile() {
+    const tabItem = tab.querySelectorAll('.tabContent .tabBtn');
+    const contentItem = tab.querySelectorAll('.tabContent .content');
+    tabItem.forEach((item, i) => {
+      function handleKeydown(e) {
+        if (e.which === 9 && !e.shiftKey && !this.classList.contains('active')) {
+          toggleAccordion(this, this.nextElementSibling);
+        } else if (e.which === 9 && e.shiftKey) {
+          toggleAccordion(this, this.nextElementSibling);
+        }
+      }
+
+      item.removeEventListener('keydown', handleKeydown);
+      item.addEventListener('keydown', handleKeydown);
+    });
+  }
+
+  function clickFunctionMobile() {
+    const tabItem = tab.querySelectorAll('.tabContent .tabBtn');
+
+    tabItem.forEach((item, index) => {
+      function clickCheck() {
+        toggleAccordion(this, this.nextElementSibling);
+      }
+
+      item.removeEventListener('click', clickCheck);
+      item.addEventListener('click', clickCheck);
+    });
+  }
+
+  function toggleAccordion(item, content) {
+    let display = window.getComputedStyle(content).display;
+    content.style.display = display;
+
+    if (display === 'none') {
+      item.classList.add('active');
+
+      display = 'block';
+      content.style.overflow = 'hidden';
+      content.style.display = display;
+      // content.classList.add('active');
+      let height = content.offsetHeight;
+      content.style.height = 0;
+      content.offsetHeight;
+      content.style.transitionProperty = 'height';
+      content.style.transitionDuration = `300ms`;
+      content.style.height = height + 'px';
+
+      setTimeout(() => {
+        content.style.removeProperty('overflow');
+        content.style.removeProperty('height');
+        content.style.removeProperty('transition-duration');
+        content.style.removeProperty('transition-property');
+      }, 300);
+    } else {
+      item.classList.remove('active');
+      content.style.overflow = 'hidden';
+      content.style.height = `${content.offsetHeight}px`;
+      content.style.transitionProperty = 'height';
+      content.style.transitionDuration = `300ms`;
+      content.offsetHeight;
+      content.style.height = 0;
+      setTimeout(() => {
+        content.style.display = 'none';
+        content.style.removeProperty('overflow');
+        content.style.removeProperty('height');
+        content.style.removeProperty('display');
+        content.style.removeProperty('transition-duration');
+        content.style.removeProperty('transition-property');
+      }, 300);
+    }
+
+    const siblings = Array.prototype.filter.call(item.parentElement.parentElement.children, (child) => {
+      return child !== item.parentElement;
+    });
+  }
+
+  function a11yControl() {
+    const tabItem = tab.querySelectorAll('.tabItems .tabBtn');
+    const contentItem = tab.querySelectorAll('.tabContent');
+    const lastFocus = [...tab.querySelectorAll('a,input,select,textarea')].pop();
+    const firstFocus = [...tab.querySelectorAll('a,input,select,textarea')].shift();
+    const firstItem = [...tab.querySelectorAll('.tabBtn')].shift();
+    tabItem.forEach((item, index) => {
+      let content = contentItem[index].querySelectorAll('a,input,select,textarea');
+      let prevItem = contentItem[index - 1] !== undefined ? contentItem[index - 1].querySelectorAll('a,input,select,textarea') : '';
+      let prevItemLastA;
+      prevItem !== undefined ? (prevItemLastA = prevItem[prevItem.length - 1]) : '';
+
+      function handleKeydown(e) {
+        if (e.which === 9 && !e.shiftKey) {
+          e.preventDefault();
+          openTarget(tabItem, contentItem, index);
+          content[0].focus();
+        } else if (e.which === 9 && e.shiftKey && e.target !== firstItem) {
+          e.preventDefault();
+          openTarget(tabItem, contentItem, index - 1);
+          if (prevItemLastA !== undefined) {
+            prevItemLastA.focus();
+          } else {
+            tabItem[index - 1].focus();
+          }
+        }
+      }
+      item.removeEventListener('keydown', handleKeydown);
+      item.addEventListener('keydown', handleKeydown);
+
+      if (content.length !== 0) {
+        function contentFirstCheck(e) {
+          if (e.which === 9 && e.shiftKey && index !== 0) {
+            e.preventDefault();
+            tabItem[index].focus();
+          } else if (e.which === 9 && e.shiftKey && e.target == firstFocus) {
+            e.preventDefault();
+            tabItem[0].focus();
+          }
+        }
+        content[0].removeEventListener('keydown', contentFirstCheck);
+        content[0].addEventListener('keydown', contentFirstCheck);
+        function contentLastCheck(e) {
+          if (e.which === 9 && !e.shiftKey && e.target !== lastFocus) {
+            tabItem[index].focus();
+          }
+        }
+        content[content.length - 1].removeEventListener('keydown', contentLastCheck);
+        content[content.length - 1].addEventListener('keydown', contentLastCheck);
+      }
+    });
+  }
+
+  function clickFunction() {
+    const tabItem = tab.querySelectorAll('.tabItems .tabBtn');
+    const contentItem = tab.querySelectorAll('.tabContent');
+    tabItem.forEach((item, index) => {
+      function clickCheck(e) {
+        openTarget(tabItem, contentItem, index);
+      }
+      item.removeEventListener('click', clickCheck);
+      item.addEventListener('click', clickCheck);
+      // }
+    });
+  }
+
+  function openTarget(tabItem, contentItem, index) {
+    let siblingContentItem = [...contentItem].filter((item, index) => item !== this);
+    tabItem.forEach((item, index) => item.classList.remove('active'));
+    siblingContentItem.forEach((item, index) => item.classList.remove('active'));
+    contentItem[index].classList.add('active');
+    tabItem[index].classList.add('active');
+  }
+
+  function desktopType() {
+    tab.classList.remove('onMobile');
+    tab.querySelector('.tabItems') ? '' : tab.prepend(desktopTabItems);
+    const contentItem = tab.querySelectorAll('.tabContent');
+    contentItem.forEach((item, index) => {
+      if (item.querySelector('.tabBtn') !== null) {
+        item.querySelector('.tabBtn').remove();
+      }
+    });
+  }
+
+  function mobileType() {
+    tab.classList.add('onMobile');
+    tab.querySelector('.tabItems') ? (desktopTabItems = tab.querySelector('.tabItems').cloneNode(true)) : '';
+    tab.classList.add('onMobile');
+    const tabItem = tab.querySelectorAll('.tabItems .tabBtn');
+    const contentItem = tab.querySelectorAll('.tabContent');
+
+    tabItem.forEach((item, i) => {
+      let btnContent = item.innerHTML;
+      let newBtn = item.cloneNode();
+      newBtn.innerHTML = btnContent;
+      contentItem[i].prepend(newBtn);
+      contentItem[i].querySelector('.content').setAttribute('role', 'tabpanel');
+      contentItem[i].querySelector('.content').setAttribute('aria-labelledby', item.attributes['aria-controls'].value);
+      contentItem[i].querySelector('.content').setAttribute('aria-label', item.innerHTML);
+
+      contentItem[i].removeAttribute('role');
+      contentItem[i].removeAttribute('aria-label');
+      contentItem[i].removeAttribute('aria-labelledby');
+    });
+
+    tab.querySelector('.tabItems') ? tab.querySelector('.tabItems').remove() : '';
+  }
+
+  init();
+  checkType();
+  function checkType() {
+    const tabItem = tab.querySelectorAll('.tabItems .tabBtn');
+    const contentItem = tab.querySelectorAll('.tabContent');
+    tabItem.forEach((item, index) => {
+      item.classList.remove('active');
+      contentItem[index].classList.remove('active');
+      contentItem[index].querySelector('.content').style.display = 'none';
+      item.setAttribute('aria-selected', false);
+    });
+    tabItem[openIndex] ? tabItem[openIndex].setAttribute('aria-selected', 'true') : '';
+    tabItem[openIndex] ? tabItem[openIndex].classList.add('active') : '';
+
+    let tabWidth = tab.offsetWidth;
+    setTimeout(() => {
+      if (tabWidth > 700) {
+        contentItem[openIndex].classList.add('active');
+        tabItem.forEach((item, index) => {
+          contentItem[index].querySelector('.content').removeAttribute('style');
+        });
+        desktopType();
+        a11yControl();
+        clickFunction();
+      } else {
+        contentItem[openIndex].querySelector('.content').style.display = 'block';
+        mobileType();
+        clickFunctionMobile();
+        a11yControlMobile();
+      }
+    }, 100);
+  }
+  window.addEventListener('resize', checkType);
+}
+TabFunction2();
